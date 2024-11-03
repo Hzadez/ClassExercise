c5719774826bbae819a8ab71e76b5b11e9e738c0
Hzadez <hzadez63@gmail.com>
2024-11-03 10:22:07 +0400

    Class Exercesi

---
+++ Class Exercise/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace Class_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student std1 = new Student()
+            {
+                Name = "Eli",
+                Surname = "Ahadov",
+                Age = 18,
+                Grant = 180,
+                GroupNo = "PB503"
+
+            };
+
+            Student std2 = new Student()
+            {
+                Name = "Dadash",
+                Surname = "Sharifov",
+                Age = 20,
+                Grant = 250,
+                GroupNo = "PB503"
+
+            };
+
+            Student std3 = new Student()
+            {
+                Name = "Kenan",
+                Surname = "Heyderov",
+                Age = 21,
+                Grant = 255,
+                GroupNo = "PB503"
+
+            };
+
+            std1.ShowFullData();
+
+            Teacher tch1 = new Teacher()
+            {
+                Name = "Veli",
+                Surname = "Qurbanov",
+                Age = 38,
+                Salary = 500,
+                GroupNo = "PB503"
+
+            };
+
+            Teacher tch2 = new Teacher()
+            {
+                Name = "Qurban",
+                Surname = "Seyidov",
+                Age = 41,
+                Salary = 1000,
+                GroupNo = "PB503"
+
+            };
+
+            Teacher tch3 = new Teacher()
+            {
+                Name = "Seyran",
+                Surname = "Melikov",
+                Age = 50,
+                Salary = 2000,
+                GroupNo = "PB503"
+
+            };
+
+            tch1.ShowFullData();
+
+        }
+    }
+
+    public class Human
+    {
+        public string Name = "Noname";
+        public string Surname = "Nosurname";
+        public byte Age;
+    }
+
+    public class Teacher : Human
+    {
+        public int Salary;
+        public string GroupNo;
+
+        public void ShowFullData()
+        {
+            Console.WriteLine($"{Name}, {Surname}, {Age}, {Salary}, {GroupNo}");
+        }
+
+
+    }
+
+    public class Student : Human
+    {
+        public byte Grant;
+        public string GroupNo;
+
+        public void ShowFullData()
+        {
+            Console.WriteLine($"{Name}, {Surname}, {Age}, {Grant}, {GroupNo}");
+        }
+
+    }
+
+}
